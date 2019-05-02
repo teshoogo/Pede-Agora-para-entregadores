25b139a6ca1299d3273ab068d8d209e8c8e983f3
weslleyth15 <50183367+weslleyth15@users.noreply.github.com>
2019-05-01 22:50:53 -0300

    Atualização

---
+++ www/js/config.js
@@ -0,0 +1,9 @@
+/*Driver App Configuration*/
+
+var krms_driver_config ={			
+	'ApiUrl':"https://pedeagora.hospteste.ga/driver/api",		
+	'DialogDefaultTitle':"DriverApp",
+	'mapboxToken' : 'pk.eyJ1Ijoid2VzbGxleXRoMTUiLCJhIjoiY2p1eGIwMmdjMGtrbDN5cnZsajFidzRuMiJ9.XW8Mz_nrCC_nT4nOM4pMcQ',
+	'APIHasKey':"asd564sad987sd52a4165sd4a8974sd5a46",
+	'debug': false
+};
